e41895e949bde43286f21b7ba277fdc6bb9a5652
Kidus Michael <kimichae@ucsc.edu>
2021-06-26 11:52:52 -0700

    Spaces added

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -3,10 +3,9 @@ class Rocket extends Phaser.GameObjects.Sprite {
     constructor(scene, x, y, texture, frame) {
       super(scene, x, y, texture, frame);
   
-      // add object to existing scene
-      scene.add.existing(this);
-      this.isFiring = false;
-      this.moveSpeed = 2;
+      scene.add.existing(this);     // add object to existing, displayList, updateList
+      this.isFiring = false;        // track rocket's firing squad
+      this.moveSpeed = 2;           // pixels per frame
     }
 
     update() {
