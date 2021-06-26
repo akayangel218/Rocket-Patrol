1a1d714079dbb4e7da4415e2df78194cd550b944
Kidus Michael <kimichae@ucsc.edu>
2021-06-26 11:05:18 -0700

    Rocket added

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -5,5 +5,32 @@ class Rocket extends Phaser.GameObjects.Sprite {
   
       // add object to existing scene
       scene.add.existing(this);
+      this.isFiring = false;
+      this.moveSpeed = 2;
+    }
+
+    update() {
+        // left/right movement
+        if(!this.isFiring) {
+            if(keyLEFT.isDown && this.x >= borderUISize + this.width) {
+                this.x -= this.moveSpeed;
+            }
+            else if (keyRIGHT.isDown && this.x <= game.config.width - borderUISize - this.width) {
+                this.x += this.moveSpeed;
+            }
+        }
+        // fire button
+        if (Phaser.Input.Keyboard.JustDown(keyF)) {
+            this.isFiring = true;
+        }
+        // if fired, move up
+        if (this.isFiring && this.y >= borderUISize * 3 + borderPadding) {
+            this.y -= this.moveSpeed;
+        }
+        // reset on miss
+        if (this.y <= borderUISize * 3 + borderPadding) {
+            this.isFiring = false;
+            this.y = game.config.height - borderUISize - borderPadding;
+        }
     }
   }
