7562532b676533e4c89e20217467ad5e361f23cb
Kidus Michael <kimichae@ucsc.edu>
2021-06-26 10:52:26 -0700

    New changes with rocket and starfields

--- src/main.js
+++ src/main.js
@@ -5,3 +5,7 @@ let config = {
     scene: [ Menu, Play ]
 }
 let game = new Phaser.Game(config);
+
+// set UI sizes
+let borderUISize = game.config.height / 15;
+let borderPadding = borderUISize / 3;
