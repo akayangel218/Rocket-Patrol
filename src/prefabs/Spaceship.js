5da0d51ddff7da7ab714188cb5d340a0c62edc65
Kidus Michael <kimichae@ucsc.edu>
2021-06-26 12:16:47 -0700

    Explosions added

--- src/prefabs/Spaceship.js
+++ src/prefabs/Spaceship.js
@@ -10,9 +10,15 @@ class Spaceship extends Phaser.GameObjects.Sprite {
     update() {
         // move spaceship left
         this.x -= this.moveSpeed;
+        
         // wrap around from left edge to right edge
         if (this.x <= 0 - this.width) {
-            this.x = game.config.width;
+            this.reset();
         }
+    }
+
+    // position reset
+    reset() {
+        this.x = game.config.width;
     }
 }
